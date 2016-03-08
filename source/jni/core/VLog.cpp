1e5ed923458c5c30c0191f1a126c66fb72a4fc77
Takashiro <takashiro@qq.com>
2016-03-08 20:54:49 +0800

    VLog supports more number types

--- source/jni/core/VLog.cpp
+++ source/jni/core/VLog.cpp
@@ -23,57 +23,87 @@ VLog::~VLog()
     delete d;
 }
 
+VLog &VLog::operator << (char ch)
+{
+    d->buffer << ch << ' ';
+    return *this;
+}
+
+VLog &VLog::operator << (char16_t ch)
+{
+    d->buffer << ch << ' ';
+    return *this;
+}
+
+VLog &VLog::operator << (char32_t ch)
+{
+    d->buffer << ch << ' ';
+    return *this;
+}
+
 VLog &VLog::operator << (short num)
 {
-    d->buffer << num << " ";
+    d->buffer << num << ' ';
     return *this;
 }
 
 VLog &VLog::operator << (ushort num)
 {
-    d->buffer << num << " ";
+    d->buffer << num << ' ';
     return *this;
 }
 
 VLog &VLog::operator << (int num)
 {
-    d->buffer << num << " ";
+    d->buffer << num << ' ';
     return *this;
 }
 
 VLog &VLog::operator << (uint num)
 {
-    d->buffer << num << " ";
+    d->buffer << num << ' ';
     return *this;
 }
 
 VLog &VLog::operator << (float num)
 {
-    d->buffer << num << " ";
+    d->buffer << num << ' ';
     return *this;
 }
 
 VLog &VLog::operator << (double num)
 {
-    d->buffer << num << " ";
+    d->buffer << num << ' ';
+    return *this;
+}
+
+VLog &VLog::operator <<(long long num)
+{
+    d->buffer << num << ' ';
+    return *this;
+}
+
+VLog &VLog::operator <<(ulonglong num)
+{
+    d->buffer << num << ' ';
     return *this;
 }
 
 VLog &VLog::operator << (const char *str)
 {
-    d->buffer << str << " ";
+    d->buffer << str << ' ';
     return *this;
 }
 
 VLog &VLog::operator << (const VString &str)
 {
-    d->buffer << str << " ";
+    d->buffer << str << ' ';
     return *this;
 }
 
 VLog &VLog::operator << (const VByteArray &str)
 {
-    d->buffer << str << " ";
+    d->buffer << str << ' ';
     return *this;
 }
 
