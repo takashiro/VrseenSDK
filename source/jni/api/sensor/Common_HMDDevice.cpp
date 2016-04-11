4a51afd56f0d45017cd19dd8555c5c22b9426db3
Takashiro <takashiro@qq.com>
2016-04-11 15:44:13 +0800

    Remove codes for other platforms

--- source/jni/api/sensor/Common_HMDDevice.cpp
+++ source/jni/api/sensor/Common_HMDDevice.cpp
@@ -1,37 +1,10 @@
-/************************************************************************************
-
-Filename    :   OVR_Common_HMDDevice.cpp
-Content     :
-Created     :
-Authors     :
-
-Copyright   :   Copyright 2014 Oculus VR, LLC. All Rights reserved.
-
-*************************************************************************************/
-
-// Should be #included from the relevant OVR_YourPlatformHere_HMDDevice.cpp
-
-/************************************************************************************
-
-Filename    :   OVR_Android_HMDDevice.h
-Content     :   Android HMDDevice implementation
-Created     :
-Authors     :
-
-Copyright   :   Copyright 2014 Oculus VR, LLC. All Rights reserved.
-
-*************************************************************************************/
-
 #include "Android_HMDDevice.h"
-
 #include "Android_DeviceManager.h"
-
 #include "Profile.h"
 
-namespace NervGear { namespace Android {
+NV_NAMESPACE_BEGIN
 
-//-------------------------------------------------------------------------------------
-// ***** HMDDeviceCreateDesc
+namespace Android {
 
 DeviceBase* HMDDeviceCreateDesc::NewDeviceInstance()
 {
@@ -65,149 +38,16 @@ bool HMDDeviceCreateDesc::GetDeviceInfo(DeviceInfo* info) const
         (info->InfoClassType != Device_None))
         return false;
 
-#if defined(NV_OS_ANDROID)
 	// LDC - Use zero data for now.
 	info->Version = 0;
     info->ProductName.clear();
-	info->Manufacturer[0] = 0;
+    info->Manufacturer.clear();
 
 	if (info->InfoClassType == Device_HMD)
     {
 		HMDInfo* hmdInfo = static_cast<HMDInfo*>(info);
         *hmdInfo = HMDInfo();
 	}
-#else
-
-    HmdTypeEnum hmdType = HmdType_DKProto;
-    char const *deviceName = "Oculus Rift DK1-Prototype";
-    if ( Is7Inch() )
-    {
-        hmdType = HmdType_DK1;
-        deviceName = "Oculus Rift DK1";
-    }
-    else if ( ResolutionInPixels.Width == 1920 )
-    {
-        // DKHD protoypes, all 1920x1080
-        if ( ScreenSizeInMeters.Width < 0.121f )
-        {
-            // Screen size 0.12096 x 0.06804
-            hmdType = HmdType_DKHDProto;
-            deviceName = "Oculus Rift DK HD";
-        }
-        else if ( ScreenSizeInMeters.Width < 0.126f )
-        {
-            // Screen size 0.125 x 0.071
-            hmdType = HmdType_DKHDProto566Mi;
-            deviceName = "Oculus Rift DK HD 566 Mi";
-        }
-        else
-        {
-            // Screen size 0.1296 x 0.0729
-            hmdType = HmdType_DKHDProto585;
-            deviceName = "Oculus Rift DK HD 585";
-        }
-    }
-    else
-    {
-        vAssert ( "Unknown HMD" );
-    }
-
-    OVR_strcpy(info->ProductName,  DeviceInfo::MaxNameLength, deviceName );
-    OVR_strcpy(info->Manufacturer, DeviceInfo::MaxNameLength, "Oculus VR");
-    info->Type    = Device_HMD;
-    info->Version = 0;
-
-    // Display detection.
-    if (info->InfoClassType == Device_HMD)
-    {
-        HMDInfo* hmdInfo = static_cast<HMDInfo*>(info);
-
-        hmdInfo->HmdType                = hmdType;
-        hmdInfo->DesktopX               = Desktop.X;
-        hmdInfo->DesktopY               = Desktop.Y;
-        hmdInfo->ResolutionInPixels     = ResolutionInPixels;
-        hmdInfo->ScreenSizeInMeters     = ScreenSizeInMeters;        // Includes ScreenGapSizeInMeters
-        hmdInfo->ScreenGapSizeInMeters  = 0.0f;
-        hmdInfo->CenterFromTopInMeters  = ScreenSizeInMeters.Height * 0.5f;
-        hmdInfo->LensSeparationInMeters = 0.0635f;
-        // TODO: any other information we get from the hardware itself should be added to this list
-
-        switch ( hmdInfo->HmdType )
-        {
-        case HmdType_DKProto:
-            // WARNING - estimated.
-            hmdInfo->Shutter.Type                             = HmdShutter_RollingTopToBottom;
-            hmdInfo->Shutter.VsyncToNextVsync                 = ( 1.0f / 60.0f );
-            hmdInfo->Shutter.VsyncToFirstScanline             = 0.000052f;
-            hmdInfo->Shutter.FirstScanlineToLastScanline      = 0.016580f;
-            hmdInfo->Shutter.PixelSettleTime                  = 0.015f; // estimated.
-            hmdInfo->Shutter.PixelPersistence                 = hmdInfo->Shutter.VsyncToNextVsync; // Full persistence
-            break;
-        case HmdType_DK1:
-            // Data from specs.
-            hmdInfo->Shutter.Type                             = HmdShutter_RollingTopToBottom;
-            hmdInfo->Shutter.VsyncToNextVsync                 = ( 1.0f / 60.0f );
-            hmdInfo->Shutter.VsyncToFirstScanline             = 0.00018226f;
-            hmdInfo->Shutter.FirstScanlineToLastScanline      = 0.01620089f;
-            hmdInfo->Shutter.PixelSettleTime                  = 0.017f; // estimated.
-            hmdInfo->Shutter.PixelPersistence                 = hmdInfo->Shutter.VsyncToNextVsync; // Full persistence
-            break;
-        case HmdType_DKHDProto:
-            // Data from specs.
-            hmdInfo->Shutter.Type                             = HmdShutter_RollingLeftToRight;
-            hmdInfo->Shutter.VsyncToNextVsync                 = ( 1.0f / 60.0f );
-            hmdInfo->Shutter.VsyncToFirstScanline             = 0.0000859f;
-            hmdInfo->Shutter.FirstScanlineToLastScanline      = 0.0164948f;
-            hmdInfo->Shutter.PixelSettleTime                  = 0.012f; // estimated.
-            hmdInfo->Shutter.PixelPersistence                 = hmdInfo->Shutter.VsyncToNextVsync; // Full persistence
-            break;
-        case HmdType_DKHDProto585:
-            // Data from specs.
-            hmdInfo->Shutter.Type                             = HmdShutter_RollingLeftToRight;
-            hmdInfo->Shutter.VsyncToNextVsync                 = ( 1.0f / 60.0f );
-            hmdInfo->Shutter.VsyncToFirstScanline             = 0.000052f;
-            hmdInfo->Shutter.FirstScanlineToLastScanline      = 0.016580f;
-            hmdInfo->Shutter.PixelSettleTime                  = 0.015f; // estimated.
-            hmdInfo->Shutter.PixelPersistence                 = hmdInfo->Shutter.VsyncToNextVsync; // Full persistence
-            break;
-        case HmdType_DKHDProto566Mi:
-#if 0
-            // Low-persistence global shutter
-            hmdInfo->Shutter.Type                             = HmdShutter_Global;
-            hmdInfo->Shutter.VsyncToNextVsync                 = ( 1.0f / 76.0f );
-            hmdInfo->Shutter.VsyncToFirstScanline             = 0.0000273f + 0.0131033f;    // Global shutter - first visible scan line is actually the last!
-            hmdInfo->Shutter.FirstScanlineToLastScanline      = 0.000f;                     // Global shutter - all visible at once.
-            hmdInfo->Shutter.PixelSettleTime                  = 0.0f;                       // <100us
-            hmdInfo->Shutter.PixelPersistence                 = 0.18f * hmdInfo->Shutter.VsyncToNextVsync;     // Confgurable - currently set to 18% of total frame.
-#else
-            // Low-persistence rolling shutter
-            hmdInfo->Shutter.Type                             = HmdShutter_RollingLeftToRight;
-            hmdInfo->Shutter.VsyncToNextVsync                 = ( 1.0f / 76.0f );
-            hmdInfo->Shutter.VsyncToFirstScanline             = 0.0000273f;
-            hmdInfo->Shutter.FirstScanlineToLastScanline      = 0.0131033f;
-            hmdInfo->Shutter.PixelSettleTime                  = 0.0f;                       // <100us
-            hmdInfo->Shutter.PixelPersistence                 = 0.18f * hmdInfo->Shutter.VsyncToNextVsync;     // Confgurable - currently set to 18% of total frame.
-#endif
-            break;
-        default: vAssert ( false ); break;
-        }
-
-
-        OVR_strcpy(hmdInfo->DisplayDeviceName, sizeof(hmdInfo->DisplayDeviceName),
-                   DisplayDeviceName.toCString());
-#if   defined(NV_OS_WIN)
-        // Nothing special for Win32.
-#elif defined(NV_OS_MAC)
-        hmdInfo->DisplayId = DisplayId;
-#elif defined(NV_OS_LINUX)
-        hmdInfo->DisplayId = DisplayId;
-#elif defined(NV_OS_ANDROID)
-#else
-#error Unknown platform
-#endif
-
-    }
-#endif
 
     return true;
 }
@@ -269,4 +109,6 @@ bool HMDDevice::setProfileName(const char* name)
     return false;
 }
 
-}} // namespace NervGear::Android
+}
+
+NV_NAMESPACE_END
