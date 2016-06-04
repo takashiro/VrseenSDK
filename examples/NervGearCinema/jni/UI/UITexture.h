32e29af01c7b7de66063bab16bec515ca36fe5ed
Takashiro <takashiro@qq.com>
2016-06-04 14:42:36 +0800

    Remove non-UTF8 encoded comments

--- examples/NervGearCinema/jni/UI/UITexture.h
+++ examples/NervGearCinema/jni/UI/UITexture.h
@@ -1,38 +1,20 @@
-/************************************************************************************
+#pragma once
 
-Filename    :   UITexture.h
-Content     :
-Created     :	1/8/2015
-Authors     :   Jim Dosé
-
-Copyright   :   Copyright 2014 Oculus VR, LLC. All Rights reserved.
-
-This source code is licensed under the BSD-style license found in the
-LICENSE file in the Cinema/ directory. An additional grant 
-of patent rights can be found in the PATENTS file in the same directory.
-
-*************************************************************************************/
-
-#if !defined( UITexture_h )
-#define UITexture_h
-
-#include "api/VEglDriver.h"
+#include "VEglDriver.h"
 
 namespace OculusCinema {
 
 class UITexture
 {
 public:
-										UITexture();
-										~UITexture();
+    UITexture();
+    ~UITexture();
 
-	void 								LoadTextureFromApplicationPackage( const char *assetPath );
+    void LoadTextureFromApplicationPackage( const char *assetPath );
 
-	int 								Width;
-	int									Height;
-	GLuint 								Texture;
+    int Width;
+    int Height;
+    GLuint Texture;
 };
 
 } // namespace OculusCinema
-
-#endif // UITexture_h
