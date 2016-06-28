c22ce64dff3fe7d6ec416dc4eff2f67c76399380
Takashiro <takashiro@qq.com>
2016-06-28 19:00:51 +0800

    Remove libjpeg from PanoPhoto

--- examples/PanoPhoto/jni/Android.mk
+++ examples/PanoPhoto/jni/Android.mk
@@ -1,14 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)					# clean everything up to prepare for a module
-LOCAL_ARM_MODE  := arm					# full speed arm instead of thumb
-LOCAL_ARM_NEON  := true					# compile with neon support enabled
-LOCAL_MODULE := jpeg
-LOCAL_SRC_FILES := libjpeg.a
-
-include $(PREBUILT_STATIC_LIBRARY)
-
-include $(CLEAR_VARS)					# clean everything up to prepare for a module
 include ../../source/import_vrlib.mk		# import VRLib for this module.  Do NOT call $(CLEAR_VARS) until after building your module.
 										# use += instead of := when defining the following variables: LOCAL_LDLIBS, LOCAL_CFLAGS, LOCAL_C_INCLUDES, LOCAL_STATIC_LIBRARIES
 
@@ -22,7 +14,5 @@ LOCAL_MODULE    := nervgearphoto		# generate oculus360photos.so
 
 LOCAL_SRC_FILES := Oculus360Photos.cpp FileLoader.cpp PanoBrowser.cpp PanoMenu.cpp PhotosMetaData.cpp OVR_TurboJpeg.cpp
 #	../../../source/jni/core/VDir.cpp
-
-LOCAL_STATIC_LIBRARIES += jpeg
 
 include $(BUILD_SHARED_LIBRARY)			# start building based on everything since CLEAR_VARS
