fdc50f5780f6a663c285fcfd4e06c316c3212e47
Takashiro <takashiro@qq.com>
2016-09-07 09:57:59 +0800

    Remove copyright comments

--- examples/PanoPhoto/jni/PanoPhoto.cpp
+++ examples/PanoPhoto/jni/PanoPhoto.cpp
@@ -1,18 +1,3 @@
-/************************************************************************************
-
-Filename    :   PanoPhoto.cpp
-Content     :
-Created     :
-Authors     :
-
-Copyright   :   Copyright 2014 Oculus VR, LLC. All Rights reserved.
-
-This source code is licensed under the BSD-style license found in the
-LICENSE file in the PanoPhoto/ directory. An additional grant
-of patent rights can be found in the PATENTS file in the same directory.
-
-*************************************************************************************/
-
 #include <VAlgorithm.h>
 
 #include "PanoPhoto.h"
