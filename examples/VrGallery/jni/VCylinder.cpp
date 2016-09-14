d4b95e437689c7476ec4b501ebbdb7d09eebc4c5
codinganywhere <zhuce201602@yeah.net>
2016-09-14 08:37:06 +0800

    #68 VCylinder

---
+++ examples/VrGallery/jni/VCylinder.cpp
@@ -0,0 +1,5 @@
+//
+// Created by yangkai on 2016/9/14.
+//
+
+#include "VCylinder.h"
---
+++ examples/VrGallery/jni/VCylinder.h
@@ -0,0 +1,20 @@
+//
+// Created by yangkai on 2016/9/14.
+//
+#pragma once
+
+#include <vglobal.h>
+#include <GLES3/gl3.h>
+
+NV_NAMESPACE_BEGIN
+
+class VCylinder {
+public:
+    VCylinder() {
+
+    }
+
+private:
+    GLuint m_program;
+};
+NV_NAMESPACE_END
